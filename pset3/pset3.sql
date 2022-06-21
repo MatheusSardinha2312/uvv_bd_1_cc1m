ce5c7bcbdeffc488f31083026d09e1125834a13c
MatheusSardinha2312 <103539177+MatheusSardinha2312@users.noreply.github.com>
2022-06-20 23:56:03 -0300

    Add files

--- pset3/pset3.sql
+++ pset3/pset3.sql
@@ -5,7 +5,7 @@ WHERE codigo_pai is null
 
 UNION ALL
 
-SELECT class.codigo, CONCAT(pf.nome || '  >>>' || class.nome), class.codigo_pai
+SELECT class.codigo, CONCAT(pf.nome || ' >>> ' || class.nome), class.codigo_pai
 FROM classificacao AS class
 INNER JOIN classificacao_p AS pf on pf.codigo = class.codigo_pai
 WHERE class.codigo_Pai is NOT null)
