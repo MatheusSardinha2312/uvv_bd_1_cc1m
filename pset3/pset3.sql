0661218d5547dd6b159ffde6dba433f432f8f264
MatheusSardinha2312 <103539177+MatheusSardinha2312@users.noreply.github.com>
2022-06-20 23:52:52 -0300

    Add files

--- pset3/pset3.sql
+++ pset3/pset3.sql
@@ -1,12 +1,14 @@
-WITH RECURSIVE classificacao_p AS ( SELECT codigo, CONCAT(nome) AS nome, codigo_pai
+WITH RECURSIVE classificacao_p
+AS ( SELECT codigo, CONCAT(nome) AS nome, codigo_pai
 FROM classificacao
 WHERE codigo_pai is null
 
 UNION ALL
 
-SELECT class.codigo, CONCAT(pf.nome || ' >>> ' || class.nome), class.codigo_pai
+SELECT class.codigo, CONCAT(pf.nome || '  >>>' || class.nome), class.codigo_pai
 FROM classificacao AS class
 INNER JOIN classificacao_p AS pf on pf.codigo = class.codigo_pai
+WHERE class.codigo_Pai is NOT null)
 SELECT codigo AS "Código", nome AS "Elementos", codigo_pai AS  "Código Pai"
 FROM classificacao_p
 ORDER BY classificacao_p.nome;
